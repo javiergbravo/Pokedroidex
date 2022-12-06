e3242d6bde28eaca8a330fdbf3877df1b52ffaf7
Javier Gutiérrez Bravo <jgbravo.developer@gmail.com>
2022-12-06 21:10:06 +0100

    fix: duplicated pokemon when scroll very fast

--- app/src/main/kotlin/com/jgbravo/pokedroidex/ui/pokemonList/composables/Pokedex.kt
+++ app/src/main/kotlin/com/jgbravo/pokedroidex/ui/pokemonList/composables/Pokedex.kt
@@ -62,7 +62,7 @@ fun PokemonList(
             pokemonList.size / 2 + 1
         }
         items(itemCount) {
-            if (it >= itemCount - 1 && !endReach) {
+            if (it >= itemCount - 1 && !endReach && !isLoading) {
                 viewModel.loadPokemonPaginated()
             }
             PokedexRow(rowIndex = it, entries = pokemonList, navController = navController)
